3d164dae326df65a95f65c4531180929ffb9b4e7
Karanpreet2001 <karanpreetfarwaha@gmail.com>
2022-03-09 11:11:15 -0800

    We chat app first commit conversation

---
+++ server/server.js
@@ -0,0 +1,25 @@
+
+
+
+const Header = () => {
+    return ( 
+        <div>
+            <header>
+                <h2>We Chat</h2>
+                <nav>
+                    <ul>
+                       <li ><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Services</a></li>
+                       
+
+                    </ul>
+                </nav>
+
+                <a><button>Contact</button></a>
+            </header>
+        </div>
+     );
+}
+ 
+export default Hedader;
